fb77eedc79629c15ad34cf6a07fae75c1edc0460
naroahlee <naroahlee@gmail.com>
2019-05-10 14:25:28 -0500

    Norm_Simulation

--- approx_norm_ver.py
+++ approx_norm_ver.py
@@ -10,9 +10,10 @@ from lib.server_model    import *
 from lib.analytics import get_MDDS1_from_BDDS1
 import matplotlib.pyplot as plt
 
+GEN_NEW_DATA = False
 # Real Value
 arrival_rate_real = 0.04
-service_dur_real  = 8.9
+service_dur_real  = 8.6
 period_real       = 10.0
 bandwidth         = 0.6
 
@@ -39,24 +40,27 @@ x_bd = x_bd[0 : int(x_lim * N)]
 y_bd = y_bd[0 : int(x_lim * N)]
 
 #================= M/D(DS)/1 Simulation ==================
-#sample_num   = 20000
+scale_when_doing_exp_in_ms = 10.0
+sample_num   = 20000
 ecdf_samples = 10000
 x_axis = np.linspace(0, x_lim, ecdf_samples)
 #
 ## Record the Data
-#processfile  = './data/input/' + runfile + '.csv'
+processfile  = './data/input/' + runfile + '.csv'
 #
-#if (True == GEN_NEW_DATA):
-#	arrival_evt = gen_poisson_process(arrival_rate, sample_num)
-#	write_arrival_data(processfile, arrival_evt)
-#else:
-#	arrival_evt = read_arrival_data(processfile)
-#
-#(atserver_evt, leave_evt) = run_D_FIFO_DS_server(budget, period, service_rate, arrival_evt)
-#response_time = np.subtract(leave_evt, arrival_evt)
-#response_time = np.array(response_time)
-#ecdf2 = sm.distributions.ECDF(response_time)
-#y_simu_ds = ecdf2(x_axis);
+if (True == GEN_NEW_DATA):
+	arrival_evt = gen_poisson_process(arrival_rate, sample_num)
+	write_arrival_data(processfile, arrival_evt)
+else:
+	arrival_evt = read_arrival_data(processfile)
+
+arrival_evt = np.array(arrival_evt)
+arrival_evt = arrival_evt * scale_when_doing_exp_in_ms / service_dur_real
+
+(atserver_evt, leave_evt) = run_D_FIFO_DS_server(budget, period, service_rate, arrival_evt)
+response_time = np.subtract(leave_evt, arrival_evt)
+ecdf2 = sm.distributions.ECDF(response_time)
+y_simu_ds = ecdf2(x_axis);
 
 #================= M/D(DS)/1 Empirical ==================
 scale = 10.0
@@ -74,15 +78,15 @@ mytitle = "Response Time CDF: P=%.2f, W=%2d%%, %c=%.2f, %c=%.2f" % (period, int(
 figwidth  = 6
 figheight = 5
 plt.figure(figsize=(figwidth, figheight))
-#plt.plot(x_axis, y_simu_ds, linestyle='-', color='blue', drawstyle='steps', clip_on=False, linewidth=2.8)
-plt.plot(x_bd, y_bd, linestyle='--', color='orange', drawstyle='steps', clip_on=False, linewidth=2.0)
-plt.plot(x_axis, y_empr_ds, linestyle='--', color='red', drawstyle='steps', clip_on=False, linewidth=2.8)
+plt.plot(x_axis, y_simu_ds, linestyle='--', color='blue', drawstyle='steps', clip_on=False, linewidth=2.0)
+plt.plot(x_bd, y_bd, linestyle='--', color='red', drawstyle='steps', clip_on=False, linewidth=2.0)
+plt.plot(x_axis, y_empr_ds, linestyle='--', color='black', drawstyle='steps', clip_on=False, linewidth=2.0)
 plt.xlabel('Normalized Response Time', fontsize = 12)
 plt.ylabel('Proportion', fontsize = 12)
 plt.title(mytitle, fontsize = 12)
 plt.xlim([0, x_lim])
 plt.ylim([0, y_lim])
-plt.legend(['Numerical approx N=%d' % (N), 'Redis Empr'])
+plt.legend(['Simulation', 'Numerical approx N=%d' % (N), 'Redis Empr'])
 #plt.legend(['M/D(DS)/1 Sim','B/D(DS)/1 approx N=%d' % (N), 'M/D(DS)/1 Empr'])
 
 plt.show()
